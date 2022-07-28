5cbe25d516fb685fd1d896feb4fc4e734cd207d3
MacGitHub2013 <bhanu.net10@gmail.com>
2022-07-28 13:23:46 +0530

    2nd commit

--- client/views.py
+++ client/views.py
@@ -1,3 +1,16 @@
+
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import Client
+from django.urls import reverse
 
 # Create your views here.
+
+class CreateClientView(CreateView):
+    model=Client
+    template_name= "clients/create_view.html"
+    # fields = Client._meta.get_fields()
+    fields = ['first_name','last_name','address','contact_no','email','gender']
+
+    def get_success_url(self):
+        return reverse('add_client')
